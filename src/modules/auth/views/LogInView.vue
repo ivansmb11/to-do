c2a781d5709e2b9ce7d56556d0227eb291d4f426
oswi <oswaldo.adrian@live.com.mx>
2022-04-23 12:03:17 -0500

    test router link

--- src/modules/auth/views/LogInView.vue
+++ src/modules/auth/views/LogInView.vue
@@ -5,5 +5,8 @@
 <template>
 	<div>
 		<p>log in</p>
+		<router-link :to="{ name: 'sign-up' }">
+			sign-up
+		</router-link>
 	</div>
 </template>
--- src/modules/auth/views/SignUpView.vue
+++ src/modules/auth/views/SignUpView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+	
 </script>
 
 <template>
 	<div>
 		<p>sign up</p>
+		<router-link :to="{ name: 'log-in' }">
+			log-in
+		</router-link>
 	</div>
 </template>
