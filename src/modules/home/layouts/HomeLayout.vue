d8b43090e516fd69d14ae60e040ad01eb02f642d
oswi <oswaldo.adrian@live.com.mx>
2022-05-11 17:41:17 -0500

    starting new todo feature

--- src/modules/home/layouts/HomeLayout.vue
+++ src/modules/home/layouts/HomeLayout.vue
@@ -57,15 +57,51 @@ getToDos();
 		</div>
 		<div class="col-2 position-relative">
 			<div class="position-absolute top-0 end-0 m-5 text-danger" style="font-size: 2rem;">
-				<i @click="onLogout" class="fas fa-sign-out-alt fa-xl"></i>
+				<i
+					@click="onLogout"
+					class="fas fa-sign-out-alt fa-xl"
+					style="cursor: pointer;">
+				</i>
 			</div>
 			<div class="position-absolute bottom-0 end-0 m-4">
-				<button class="btn btn-circle btn-primary">
+				<button
+					class="btn btn-circle btn-primary"
+					data-bs-toggle="modal"
+					data-bs-target="#newToDo">
 					<i class="fas fa-plus"></i>
 				</button>
 			</div>
 		</div>
-
+	</div>
+	<div class="modal fade" id="newToDo" tabindex="-1" aria-labelledby="newToDoLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="newToDoLabel">new to-do:_</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<form>
+						<div class="mb-3">
+							<label for="title" class="col-form-label">title</label>
+							<input type="text" class="form-control" id="title">
+						</div>
+						<div class="mb-3">
+							<label for="date" class="col-form-label">date</label>
+							<input type="text" class="form-control" id="date">
+						</div>
+						<div class="mb-3">
+							<label for="description" class="col-form-label">description</label>
+							<textarea class="form-control" id="description"></textarea>
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">close</button>
+					<button type="button" class="btn btn-primary">save</button>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 </template>
